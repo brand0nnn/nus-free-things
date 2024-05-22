f0a31e37db3c3cbd48bd09b58aa8b845a9009df6
Brandon Kang <notbrandonkang@gmail.com>
2024-05-22 23:02:52 +0800

    firebase changes

---
+++ NUS_Free_Things/app/firebase.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
